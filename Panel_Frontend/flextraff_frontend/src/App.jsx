b89154c7de39723f94df08c4a6dc2f4cb8ffd446
Manan Bagga <mananbagga0310@gmail.com>
2025-09-11 15:17:10 +0530

    Initial project setup

---
+++ Panel_Frontend/flextraff_frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-center bg-gray-500 p-10'>Hey Money Coming!</h1>
+  )
+}
+
+export default App
